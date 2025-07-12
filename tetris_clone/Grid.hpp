3107e71d7a485d6751f3fb41a041beb732114af7
MirceaStefoni <mircea.stefoni82@yahoo.ro>
2025-07-12 21:14:40 +0300

    Initial commit, create general project structure

---
+++ tetris_clone/Grid.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef _GRID_H_
+#define _GRID_H_
+
+#include <iostream>
+#include <vector>
+
+class Grid {
+private:
+
+	int rows = 20;
+	int cols = 10;
+
+	std::vector<std::vector<int>> grid;
+
+
+public:
+
+
+
+};
+
+
+
+#endif // !_GRID_H_
